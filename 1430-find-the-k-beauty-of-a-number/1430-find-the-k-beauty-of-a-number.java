f9b63c7e9c41189386bb39f92ae25a29e4c9697b
Reena S <160510029+Reena-senthilkumar@users.noreply.github.com>
2025-07-23 14:31:18 +0530

    Time: 0 ms (100%), Space: 40.5 MB (59.09%) - LeetHub

--- 1430-find-the-k-beauty-of-a-number/1430-find-the-k-beauty-of-a-number.java
+++ 1430-find-the-k-beauty-of-a-number/1430-find-the-k-beauty-of-a-number.java
@@ -3,8 +3,8 @@ class Solution {
         String s=String.valueOf(num);
         int c=0;
         for(int i=0;i<=s.length()-k;i++){
-            String sub=s.substring(i,k+i);
-            int div=Integer.parseInt(sub);
+            // String sub=s.substring(i,k+i);
+            int div=Integer.parseInt(s.substring(i,k+i));
         if(div!=0 && num % div==0){
             c++;
         }
