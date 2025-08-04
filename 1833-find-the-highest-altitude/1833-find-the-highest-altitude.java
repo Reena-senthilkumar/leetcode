df94c25228d434d0064d0c978db190bf746f4a63
Reena S <160510029+Reena-senthilkumar@users.noreply.github.com>
2025-08-04 14:14:16 +0530

    Time: 0 ms (100%), Space: 41.6 MB (32.5%) - LeetHub

--- 1833-find-the-highest-altitude/1833-find-the-highest-altitude.java
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.java
@@ -3,7 +3,7 @@ class Solution {
         int sum=0;
         int altitude=0;
         for(int i:gain){ //-5  //1 //5 //0 //-7
-            sum=sum+i; //0+-5=-5 //-5+1=-4//-4+5=1 //1+0=1
+            sum+=i; //0+-5=-5 //-5+1=-4//-4+5=1 //1+0=1
             if(sum>0){ //1 //1
                 altitude=Math.max(sum,altitude);//max(1,0)=1 //max(1,1)
             }
