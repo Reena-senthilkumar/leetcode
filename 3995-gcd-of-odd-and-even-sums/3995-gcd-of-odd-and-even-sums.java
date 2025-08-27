77008be84533ed1967a70ad3d6aa3abec25220c3
Reena S <160510029+Reena-senthilkumar@users.noreply.github.com>
2025-08-27 12:49:15 +0530

    Time: 0 ms (100%), Space: 40.8 MB (79.54%) - LeetHub

--- 3995-gcd-of-odd-and-even-sums/3995-gcd-of-odd-and-even-sums.java
+++ 3995-gcd-of-odd-and-even-sums/3995-gcd-of-odd-and-even-sums.java
@@ -1,20 +1,20 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        int a=0;
-        int b=0;
-        for(int i=1;i<=2*n;i++){
-            if(i%2!=0){
-            a+=i;
-            }
-            else{
-                b+=i;
-            }
-        }
-        while (b != 0) {
-            int temp = b;
-            b = a % b; 
-            a = temp;
-        }
-        return a;
+        // int a=0;
+        // int b=0;
+        // for(int i=1;i<=2*n;i++){
+        //     if(i%2!=0){
+        //     a+=i;
+        //     }
+        //     else{
+        //         b+=i;
+        //     }
+        // }
+        // while (b != 0) {
+        //     int temp = b;
+        //     b = a % b; 
+        //     a = temp;
+        // }
+        return n;
     }
 }
