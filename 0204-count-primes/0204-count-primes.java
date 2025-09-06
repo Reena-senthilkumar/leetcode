c080a30d64a6906bb51d4f3bbe2c02266a60a60a
Reena S <160510029+Reena-senthilkumar@users.noreply.github.com>
2025-09-06 11:19:35 +0530

    Time: 95 ms (66.62%), Space: 49 MB (84.16%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -8,11 +8,13 @@ class Solution {
         isprime[1]=false;
 
         for(int i=2;i*i<n;i++){
+            if(isprime[i]){
             for(int j=i*i;j<n;j+=i){
                     isprime[j]=false;
                 }
             }
-
+        }
+              
         for(boolean prime:isprime){
             if(prime) count++;
         }
