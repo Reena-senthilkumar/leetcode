d75702e0d4e659e22ff750ab022739e036292c73
Reena S <160510029+Reena-senthilkumar@users.noreply.github.com>
2025-09-20 19:18:22 +0530

    Time: 5 ms (83.79%), Space: 41.8 MB (73.9%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -5,11 +5,9 @@ class Solution {
         int i = 0;
         while (i < nums.length) {
             int start = i;
-            // move i until the sequence breaks
             while (i + 1 < nums.length && nums[i + 1] == nums[i] + 1) {
                 i++;
             }
-            // if start == i, it's a single number
             if (start == i) {
                 list.add(String.valueOf(nums[start]));
             } else {
