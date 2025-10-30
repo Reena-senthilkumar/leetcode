d1bdca53e6e189fb80a46ff62aa18a402c5b6853
Reena S <160510029+Reena-senthilkumar@users.noreply.github.com>
2025-10-30 15:23:52 +0530

    Time: 0 ms (100%), Space: 40.7 MB (66.06%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int trailingZeroes(int n) {
+        int count=0; int temp=5;
+        while(n>=temp){
+           count+=(n/temp);
+           temp*=5;
+        }
+        return count;
+    }
+}
