43f1cad5c694e81ecd2ea2c847fb04aecd3490a8
Reena S <160510029+Reena-senthilkumar@users.noreply.github.com>
2026-01-07 11:21:08 +0530

    Time: 26 ms (71.75%), Space: 46.9 MB (73.58%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,27 +1,17 @@
 class Solution {
     public String removeDuplicates(String s, int k) {
-
-        // Stack stores: character + its consecutive count
         Stack<int[]> stack = new Stack<>();
-
         for (char c : s.toCharArray()) {
-
-            // if same as previous character
             if (!stack.isEmpty() && stack.peek()[0] == c) {
                 stack.peek()[1]++;
-
-                // if count reaches k → remove
                 if (stack.peek()[1] == k) {
                     stack.pop();
                 }
             } 
             else {
-                // new character starts with count = 1
                 stack.push(new int[]{c, 1});
             }
         }
-
-        // build answer
         StringBuilder sb = new StringBuilder();
         for (int[] pair : stack) {
             sb.append(String.valueOf((char) pair[0]).repeat(pair[1]));
